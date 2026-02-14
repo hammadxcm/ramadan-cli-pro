3d02bc27fd1787c29dd036bd997718a81dabd28f
hammadxcm <hammadkhanxcm@gmail.com>
2026-02-14 11:00:00 +0500

    feat: add i18n barrel with i18next initialization

---
+++ src/i18n/index.ts
@@ -0,0 +1,7 @@
+/**
+ * @module i18n
+ * @description Barrel re-export of the internationalization service and locale types.
+ */
+
+export { I18nService } from "../services/i18n.service.js";
+export type { SupportedLocale } from "../services/i18n.service.js";
